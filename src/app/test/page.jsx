5fa4e23b1ccac9ed1ff52e9406e1805c3b132c20
svnmurali-2004 <123376873+svnmurali-2004@users.noreply.github.com>
2025-06-26 14:36:57 +0530

    mvp modifies

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,10 +1,9 @@
-import ReceiptDemo from "@/components/Receipt";
+
 
 export default function TestPage() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Test Page</h1>
-      <ReceiptDemo />
     </div>
   );
 }
